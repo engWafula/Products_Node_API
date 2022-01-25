78a07dfe1eae0f4c6d4851eedd9d76aff7006f92
engWafula <wafulaallan5@gmail.com>
2022-01-25 11:12:19 +0300

    getting  the  amazon link

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const  request =require("request-promise")
 
 const app=express()
 const PORT =process.env.PORT||5000
-
+const apiKey="b43e91fb824752147e573249b55d477a"
+const BaseUrl=`http://api.scraperapi.com?api_key=${apiKey}`
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    
+    res.send('THIS  IS  PRODUCTS  API  ')
+})
+
+//  get  product details
+app.get('/products/:productId',async(req,res)=>{
+    const {productId}=req.params
+    try {
+        const response= await request(`${BaseUrl}&url=https://www.amazon.com/dp/${productId}`)
+        res.json(response)
+    } catch (error) {
+        res.json(error)
+    }
+})
+app.listen(PORT,()=>{
+    console.log(`Server running  at port ${PORT}&autoparse=true`)
 })
