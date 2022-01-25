ab55495a2cb054de30a5732f6425952820c2f948
engWafula <wafulaallan5@gmail.com>
2022-01-25 11:58:39 +0300

    more  improvements

--- index.js
+++ index.js
@@ -16,11 +16,46 @@ app.get('/products/:productId',async(req,res)=>{
     const {productId}=req.params
     try {
         const response= await request(`${BaseUrl}&url=https://www.amazon.com/dp/${productId}`)
-        res.json(response)
+        res.json(JSON.parse(response))
     } catch (error) {
         res.json(error)
     }
 })
+
+//get  product  reviews
+
+app.get('/products/:productId/reviews',async(req,res)=>{
+    const {productId}=req.params
+    try {
+        const response= await request(`${BaseUrl}&url=https://www.amazon.com/product-reviews/${productId}`)
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+//get  product offers
+app.get('/products/:productId/offers',async(req,res)=>{
+    const {productId}=req.params
+    try {
+        const response= await request(`${BaseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+//serach  query
+app.get('/search/:searchQuery',async(req,res)=>{
+    const {productId}=req.params
+    try {
+        const response= await request(`${BaseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`)
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
 app.listen(PORT,()=>{
     console.log(`Server running  at port ${PORT}&autoparse=true`)
 })
