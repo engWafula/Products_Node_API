8c8e0a555bd42f0bb6ec31e50df5e4adb24e7a54
engWafula <wafulaallan5@gmail.com>
2022-01-25 10:45:19 +0300

    first  work

---
+++ index.js
@@ -0,0 +1,11 @@
+const  express =require("express")
+const  request =require("request-promise")
+
+const app=express()
+const PORT =process.env.PORT||5000
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    
+})
